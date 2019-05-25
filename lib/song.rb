646ec923510d64dcc30d62f5c4800656b8128eb3
ryan m <contact@ryanmeek.com>
2019-05-25 15:05:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,19 @@
 class Song
-  attr_accessor :title
- 
+  attr_accessor :name, :artist
+
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
   end
 
 end
