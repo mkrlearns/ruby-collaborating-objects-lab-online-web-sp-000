679e0e5608a9de285c9e56a59e108c42ff622dc3
ryan m <contact@ryanmeek.com>
2019-05-25 15:04:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,8 @@
 class Song
   attr_accessor :title
  
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
-  end
-
-  def artist_name=(name)
-    self.artist.nil 
-      ? self.artist = Artist.new(name)
-      : self.artist.name = name
+  def initialize(name)
+    @name = name
   end
 
 end
